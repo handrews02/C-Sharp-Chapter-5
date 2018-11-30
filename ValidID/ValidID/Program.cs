c1541dc791710696b37facfd736960be66662ac7
Hannah Andrews <handrews02@users.noreply.github.com>
2018-11-30 12:54:42 -0600

    update text

--- ValidID/ValidID/Program.cs
+++ ValidID/ValidID/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ValidID
 {
@@ -10,6 +11,24 @@ namespace ValidID
     {
         static void Main(string[] args)
         {
+            int idNum;
+            string input;
+            const int LOW = 1000;
+            const int HIGH = 9999;
+
+            Write("Enter an ID number >> ");
+            input = ReadLine();
+            idNum = Convert.ToInt32(input);
+
+            while(idNum <LOW || idNum > HIGH)
+            {
+                WriteLine(idNum + " is an invalid ID number");
+                Write("ID number must be between {0} and {1} inclusive", LOW, HIGH);
+                Write("Enter an ID number >> ");
+                input = ReadLine();
+                idNum = Convert.ToInt32(input);
+            }
+            WriteLine("ID number {0} is valid.", idNum);
         }
     }
 }
