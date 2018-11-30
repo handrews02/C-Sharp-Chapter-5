78164da5973242e9d50f5d767478e97950379c69
Hannah Andrews <handrews02@users.noreply.github.com>
2018-11-30 12:41:52 -0600

    update text

--- messinWithLoops/messinWithLoops/Program.cs
+++ messinWithLoops/messinWithLoops/Program.cs
@@ -10,6 +10,23 @@ namespace messinWithLoops
     {
         static void Main(string[] args)
         {
+            int i = 0;
+            int j = 5;
+
+            while ( i <= 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (j >= 0)
+            {
+                Console.WriteLine(j);
+                j--;
+            }
+            for(int x = 0; x <= 5; x++)
+            {
+                Console.WriteLine(x);
+            }
         }
     }
 }
