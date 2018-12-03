3bd5d731ec586fc43a1175581ed48f15b109b87b
Hannah Andrews <handrews02@users.noreply.github.com>
2018-12-03 12:21:49 -0600

    update text

---
+++ SumInts/SumInts/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SumInts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int sum = 0;
+            string inputString;
+        
+            while(num != 999)
+            {
+                WriteLine("Enter a number or 999 to exit >>>");
+                inputString = ReadLine();
+                num = Convert.ToInt32(inputString);
+                sum = (sum + num) - 999;
+            }
+            WriteLine("The sum is " + sum);
+        }
+    }
+}
