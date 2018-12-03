42347737c16b5946874ed622f9656b15626fdb2e
Hannah Andrews <handrews02@users.noreply.github.com>
2018-12-03 12:06:29 -0600

    update text

---
+++ SumFourInts/SumFourInts/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SumFourInts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int sum = 0;
+            const int MAX = 4;
+
+            string inputString;
+
+            for (int i =0; i < MAX; i++)
+            {
+                WriteLine("Enter a number>>>> ");
+                inputString = ReadLine();
+                num = Convert.ToInt32(inputString);
+                sum = sum + num;
+            }
+            WriteLine("The sum is " + sum);
+
+        }
+    }
+}
