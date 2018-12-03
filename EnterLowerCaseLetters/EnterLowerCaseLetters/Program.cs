a1044924a836b5a5a3d96598ba594a9b210b1022
Hannah Andrews <handrews02@users.noreply.github.com>
2018-12-03 12:37:05 -0600

    update text

---
+++ EnterLowerCaseLetters/EnterLowerCaseLetters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterLowerCaseLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = '\0';
+            string inputLetter = null;
+
+            while(letter != '!')
+            {
+                Console.WriteLine("Enter a lowercase letter >>> ");
+                inputLetter = Console.ReadLine();
+                letter = Convert.ToChar(inputLetter);
+                if(letter >= 'a' && letter <= 'z')
+                {
+                    Console.WriteLine("OK");
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                }
+            }
+        }    
+    }
+}
