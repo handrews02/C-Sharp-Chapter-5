b1f489ded16deacc74f6943c10a8cf9320caab7a
Hannah Andrews <handrews02@users.noreply.github.com>
2018-12-04 13:50:03 -0600

    update text

--- HomeSales/HomeSales/Program.cs
+++ HomeSales/HomeSales/Program.cs
@@ -14,31 +14,59 @@ namespace HomeSales
             string inputName= null;
             string inputSales = null;
             char name = '\0';
-            double sales;
+            double dSales = 0;
+            double eSales = 0;
+            double fSales = 0;
             double dTotal = 0;
             double eTotal = 0;
             double fTotal = 0;
+            double total = 0;
+
 
             while(name != 'Z' || name !='z')
             {
-                WriteLine("Sales Associates, please enter yur first initial >>> ");
+                WriteLine("Sales Associates, please enter your first initial >>> ");
                 inputName = ReadLine();
                 name = Convert.ToChar(inputName);
                 if(name == 'd' || name == 'D')
                 {
                     WriteLine("Enter your sales or 'z' to exit");
                     inputSales = ReadLine();
-                    sales = Convert.ToDouble(inputSales);
-                    dTotal = dTotal + sales;
+                    dSales = Convert.ToDouble(inputSales);
+                    dTotal = dTotal + dSales;
+                    WriteLine("Your total is ${0}", dTotal);
+                   
                 }
                 else if(name == 'e' || name == 'E')
                 {
                     WriteLine("Enter your sales or 'z' to exit");
                     inputSales = ReadLine();
-                    sales = Convert.ToDouble(inputSales);
-                    eTotal = eTotal + sales;
+                    eSales = Convert.ToDouble(inputSales);
+                    eTotal = eTotal + eSales;
+                    WriteLine("Your total is ${0}", eTotal);
+                   
                 }
+                else if(name == 'f' || name == 'F')
+                {
+                    WriteLine("Enter your sales or 'z' to exit");
+                    inputSales = ReadLine();
+                    fSales = Convert.ToDouble(inputSales);
+                    fTotal = fTotal + fSales;
+                    WriteLine("Your total is ${0}", fTotal);
+                    
+                }
+                else
+                {
+                    WriteLine("ERROR Sales Associates, please enter yur first initial >>>  ");
+                }
+
             }
+            if( name == 'z' || name == 'Z')
+            {
+                return;
+            }
+            total = eTotal + fTotal + dTotal;
+            WriteLine("Your total is " + total);
         }
     }
 }
---
+++ HomeSales2/HomeSales2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace HomeSales2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            string inputName = null;
+            string inputSales = null;
+            char name = '\0';
+            double sales = 0;
+            double dTotal = 0;
+            double eTotal = 0;
+            double fTotal = 0;
+            double total;
+
+            //get input
+                WriteLine("Sales Associates, enter your initals >>> ");
+                inputName = ReadLine();
+                name = Convert.ToChar(inputName);
+
+            //test for conditions
+            while (name != 'Z' && name != 'z')
+            {
+               
+                WriteLine("Sales Associates, enter your sales >>> ");
+                inputSales = ReadLine();
+                sales = Convert.ToDouble(inputSales);
+
+
+                if (name == 'd' || name == 'D')
+                    dTotal = dTotal + sales;
+                else if (name == 'e' || name == 'E')
+                    eTotal = eTotal + sales;
+                else if (name == 'f' || name == 'F')
+                    fTotal = fTotal + sales;
+
+                WriteLine("Sales Associates, enter your initals >>> ");
+                inputName = ReadLine();
+                name = Convert.ToChar(inputName);
+            }   
+
+            //write output
+            total = dTotal + eTotal + fTotal;
+            WriteLine("F: Your total is ${0}", fTotal);
+            WriteLine("E: Your total is ${0}", eTotal);
+            WriteLine("D: Your total is ${0}", dTotal);
+            WriteLine("Your grand total is ${0}", total);
+
+
+
+        }
+    }
+}
