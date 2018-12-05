596e30ca8baf12bc6f2ae0c2b08d2283772e8304
Hannah Andrews <handrews02@users.noreply.github.com>
2018-12-05 13:12:20 -0600

    update text

---
+++ GuessingGame3/GuessingGame3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace GuessingGame3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guess = 12;
+            int count = 0;
+            int min = 1;
+            int max = 11;
+
+
+            Random ranNumberGenerator = new Random();
+            int randomNumber;
+            randomNumber = ranNumberGenerator.Next(min, max);
+
+
+
+            while (guess != randomNumber)
+            {
+                Console.WriteLine("Enter a number between 1 and 10 >> ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess < randomNumber)
+                {
+                    Console.WriteLine("Higher");
+                    count++;
+
+                }
+                else if (guess > randomNumber)
+                {
+                    Console.WriteLine("Lower");
+                    count++;
+                }
+                else if (guess < 0 || guess > 10)
+                {
+                    Console.WriteLine("ERROR!");
+                }
+
+            }
+            if (guess == randomNumber)
+            {
+                Console.WriteLine("You are correct! It took you {0} times to guess {1}", count + 1, randomNumber);
+            }
+        }
+    }
+}
