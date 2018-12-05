bd6fd061d7c669beba230dbae917eeaf51c64d7f
Hannah Andrews <handrews02@users.noreply.github.com>
2018-12-05 12:49:33 -0600

    update text

---
+++ OddNums/OddNums/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace OddNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <=(99 + 1); i ++)
+            {
+                if(i % 2 != 0)
+                {
+                    Write(i.ToString());
+
+                }
+                Write();
+            }
+
+        }
+    }
+}
