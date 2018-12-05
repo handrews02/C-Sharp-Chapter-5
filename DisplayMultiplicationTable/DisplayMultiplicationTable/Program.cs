a95e59367de383df40ac4aac5628e6b914c531e6
Hannah Andrews <handrews02@users.noreply.github.com>
2018-12-05 12:39:55 -0600

    update text

---
+++ DisplayMultiplicationTable/DisplayMultiplicationTable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DisplayMultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total;
+            for(int i = 1; i <=10; i++)
+            {
+                
+                for(int j = 1; j <= 10; j++)
+                {
+                    
+                    total = i * j;
+                    Write("{0,5}", (total));
+                }
+                WriteLine();
+            }
+            
+            
+        }
+        
+    }
+}
